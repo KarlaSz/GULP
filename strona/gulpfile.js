bbb8466b878b0552a8342bd9a9933252cd86f300
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2023-10-18 13:04:08 +0200

    gulpfile settings

--- strona/gulpfile.js
+++ strona/gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
 const autoprefixer = require("gulp-autoprefixer");
@@ -7,11 +7,17 @@ const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const imagemin = require("gulp-imagemin");
 const sourcemaps = require("gulp-sourcemaps");
+const clean = require("gulp-clean");
+const kit = require("gulp-kit");
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
 
 const paths = {
+  html: "./html/**/*.kit",
   sass: "./src/sass/**/*.scss",
   js: "./src/js/**/*.js",
   img: "./src/img/*",
+  dist: "./dist",
   sassDist: "./dist/css",
   jsDist: "./dist/js",
   imgDist: "./dist/img",
@@ -45,6 +51,36 @@ function convertImg(done) {
   done();
 }
 
+function handleKits(done) {
+  src(paths.html).pipe(kit()).pipe(dest("./"));
+  done();
+}
+
+function cleanStuff(done) {
+  src("./dist/img", { read: false }).pipe(clean());
+  done();
+}
+
+function startBrowserSync(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  done();
+}
+function watchForChanges(done) {
+  watch("./*.html").on("change", reload);
+  watch(
+    [paths.html, paths.sass, paths.js],
+    parallel(handleKits, sassCompiler, javaScript)
+  ).on("change", reload);
+  watch(paths.img, convertImg).on("change", reload);
+  done();
+}
+
 /* exports.sassCompiler = sassCompiler; */
 
-exports.default = series(sassCompiler, javaScript, convertImg);
+const mainfunction = parallel(handleKits, sassCompiler, javaScript, convertImg);
+exports.cleanStuff = cleanStuff;
+exports.default = series(mainfunction, startBrowserSync, watchForChanges);
